e43c75589d5ae6bc32fd98562b554a473c102163
pravatnath473121 <pravatnath473121@gmail.com>
2022-07-16 17:30:28 +0530

    selenium logical

---
+++ logical program/src/SwapTwoNumbers.java
@@ -0,0 +1,16 @@
+
+public class SwapTwoNumbers {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		System.out.println("a-"+a+" b-"+b);
+		int c=0;  
+		c=a;
+		a=b;
+		b=c;
+		System.out.println("a-"+a+" b-"+b);
+
+		
+	}
+
+}
